8e669e97927f6f9a89146c1746e03273c364836e
armano2025 <hen@brawnshtein.co.il>
2025-08-19 20:20:56 +0300

    Update config.js

--- branches/js/config.js
+++ branches/js/config.js
@@ -1,9 +1,8 @@
-<!-- /js/config.js -->
-<script>
-  window.APP_CONFIG = {
-    SHEETS_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbwnNKY0BeO5N2O9oWmr8ltCHr9fVdDdrXsL_3erastx4qdEqhCz1mYAhkvZETF-86CGnw/exec',
-    PRICES:   { group: 80, triple: 100, private: 160 },
-    PLAN_HE:  { group: 'קבוצה', triple: 'טריפל', private: 'פרטי' },
-    SUBJECTS: ['אנגלית','מתמטיקה','פיזיקה','שפה','אנגלית מדוברת']
-  };
-</script>
+// /js/config.js
+window.APP_CONFIG = {
+  SHEETS_WEBAPP_URL:
+    'https://script.google.com/macros/s/AKfycbwnNKY0BeO5N2O9oWmr8ltCHr9fVdDdrXsL_3erastx4qdEqhCz1mYAhkvZETF-86CGnw/exec',
+  PRICES:   { group: 80, triple: 100, private: 160 },
+  PLAN_HE:  { group: 'קבוצה', triple: 'טריפל', private: 'פרטי' },
+  SUBJECTS: ['אנגלית', 'מתמטיקה', 'פיזיקה', 'שפה', 'אנגלית מדוברת']
+};
